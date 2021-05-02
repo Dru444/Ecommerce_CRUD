397402a9fe77ba46034d572fc83fa3e8328db5c2
Aman Mehra <65860158+Dru444@users.noreply.github.com>
2021-05-02 16:58:36 +0530

    CSS Updated

--- Ecommerce_CRUD/products/models.py
+++ Ecommerce_CRUD/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django import forms
 from datetime import datetime
+from django.contrib.postgres.fields import ArrayField
 # Create your models here.
 OFFERS = [('10% OFF WITH CREDIT CARDS','10% OFF WITH CREDIT CARDS'),('RS.100 CASHBACK','RS.100 CASHBACK'),('FREE SHIPPING','FREE SHIPPING'),]
 CATEGORY =[('Mobile Phone','MOBILE PHONE'),('Clothing','CLOTHING'),('Laptop','LAPTOP'),('Food','FOOD')]
@@ -15,8 +16,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6,decimal_places=2)
     availablity = models.BooleanField(default=False)
     warranty = models.CharField(default='No Warrnaty',max_length=50) 
-    offers = models.CharField(max_length=200,blank=True)
-    
+    offers = ArrayField(models.CharField(max_length=200),size = 3)
+     
+     
     
 
     def __str__(self):
