686021e431201ad5ae8a985d3d321de8927fc005
Aman Mehra <65860158+Dru444@users.noreply.github.com>
2021-05-02 18:58:15 +0530

    Updated Templates

--- Ecommerce_CRUD/products/models.py
+++ Ecommerce_CRUD/products/models.py
@@ -6,6 +6,7 @@ from django.contrib.postgres.fields import ArrayField
 OFFERS = [('10% OFF WITH CREDIT CARDS','10% OFF WITH CREDIT CARDS'),('RS.100 CASHBACK','RS.100 CASHBACK'),('FREE SHIPPING','FREE SHIPPING'),]
 CATEGORY =[('Mobile Phone','MOBILE PHONE'),('Clothing','CLOTHING'),('Laptop','LAPTOP'),('Food','FOOD')]
 WARRANTY =[('No Warranty','No Warranty'),('3 Months Warranty','3 Months Warranty'),('6 Months Warranty','6 Months Warranty'),('1 Year Warranty','1 Year Warranty')]
+
 class Product(models.Model):
     name = models.CharField(max_length=300)
     description = models.TextField()
@@ -13,7 +14,7 @@ class Product(models.Model):
     category = models.CharField(choices=CATEGORY,max_length=100,blank=True)
     manufacturer = models.CharField(max_length=300,blank=True)
     date_manufactured = models.DateField(default=datetime.now)
-    price = models.DecimalField(max_digits=6,decimal_places=2)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
     availablity = models.BooleanField(default=False)
     warranty = models.CharField(default='No Warrnaty',max_length=50) 
     offers = ArrayField(models.CharField(max_length=200),size = 3)
